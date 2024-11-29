dfb8e68c4d8d34dd90d8bea61a4ae81da452577f
RengelBott <git013354@gmail.com>
2024-11-29 13:14:42 -0600

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;RengelSxnt;;\nFN:RengelSxnt\nORG:RengelSxnt\nTITLE:\nTEL;waid=15798921335:5218261275256\nX-ABLabel:RengelSxnt\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Jeffx;;\nFN:Jeffx\nORG:Jeffx\nTITLE:\nTEL;waid=17207568895:5218261275256\nX-ABLabel:Jeffx\nEND:VCARD`
   
   await conn.sendMessage(m.chat, { 
     contacts: { 
-      displayName: 'RengelSxnt', 
+      displayName: 'Jeffx', 
       contacts: [{ vcard }] 
     }
   }, { quoted: m })
