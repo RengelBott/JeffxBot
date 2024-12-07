ce5bbe1d1b304f9463e0b0f808f5d16c757b1f23
RengelBott <git013354@gmail.com>
2024-12-06 18:56:35 -0600

    Update PlayFino.js

--- plugins/PlayFino.js
+++ plugins/PlayFino.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
             additionalText = '𝐝𝐞𝐥 𝐯𝐢𝐝𝐞𝐨'
         }
         
-        let nomeDelBot = global.db.data.nomedelbot || `𝐁𝐢𝐱𝐛𝐲𝐁𝐨𝐭-𝐌𝐝 🔮`
+        let nomeDelBot = global.db.data.nomedelbot || ``
         
 const BixbyChar = (str) => {
 return str.split('').map(char => {
